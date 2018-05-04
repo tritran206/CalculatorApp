d936dd3b8560d095fef8f12b9ab77990215f7e67
Tri Tran <tritran20@gmail.com>
2018-05-03 19:05:02 -0700

    Added listeners to buttons and fixed bugs

--- app/src/main/java/com/tritran/android/calculatorapp/MainActivity.java
+++ app/src/main/java/com/tritran/android/calculatorapp/MainActivity.java
@@ -1,13 +1,169 @@
 package com.tritran.android.calculatorapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import java.text.DecimalFormat;
+import com.tritran.android.calculatorapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    //creates instance of ActivityMainBinding to use Data Binding which allows us to refer to our widgets rather then find them by id.
+    //ActivityMainBinding class was auto created when databinding was enabled in gradle.
+    private ActivityMainBinding binding;
+
+    //holds numbers that will be operated on
+    private double valueOne = Double.NaN;
+    private double valueTwo;
+
+    //creates instance of DecimalFormat to format our result as "#.##########"
+    private DecimalFormat decimalFormat;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        decimalFormat = new DecimalFormat("#.##########");
+
+        //sets onlcick listeners to each button we created
+        binding.buttonDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + ".");
+            }
+        });
+
+        binding.buttonZero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "0");
+            }
+        });
+
+        binding.buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "1");
+            }
+        });
+
+        binding.buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "2");
+            }
+        });
+
+        binding.buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "3");
+            }
+        });
+
+        binding.buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "4");
+            }
+        });
+
+        binding.buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "5");
+            }
+        });
+
+        binding.buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "6");
+            }
+        });
+
+        binding.buttonSeven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "7");
+            }
+        });
+
+        binding.buttonEight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "8");
+            }
+        });
+
+        binding.buttonNine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.editText.setText(binding.editText.getText() + "9");
+            }
+        });
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.infoTextView.setText(decimalFormat.format(valueOne) + "+");
+                binding.editText.setText(null);
+            }
+        });
+
+        binding.buttonSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.infoTextView.setText(decimalFormat.format(valueOne) + "-");
+                binding.editText.setText(null);
+            }
+        });
+
+        binding.buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.infoTextView.setText(decimalFormat.format(valueOne) + "*");
+                binding.editText.setText(null);
+            }
+        });
+
+        binding.buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.infoTextView.setText(decimalFormat.format(valueOne) + "/");
+                binding.editText.setText(null);
+            }
+        });
+
+        binding.buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.infoTextView.setText(binding.infoTextView.getText().toString() +
+                        decimalFormat.format(valueTwo) + " = " + decimalFormat.format(valueOne));
+                valueOne = Double.NaN;
     }
+});
+
+        binding.buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(binding.editText.getText().length() > 0) {
+                    CharSequence currentText = binding.editText.getText();
+                    binding.editText.setText(currentText.subSequence(0, currentText.length()-1));
+                }
+                else {
+                    valueOne = Double.NaN;
+                    valueTwo = Double.NaN;
+                    binding.editText.setText("");
+                    binding.infoTextView.setText("");
+                }
+            }
+        });
+    }
+
+
 }
